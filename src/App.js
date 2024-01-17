f1d23b2c7ef8f1434f10fa34dc91c1d278bc4101
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-01-17 01:14:49 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'; // Import the compiled Tailwind CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-white p-4">
+      Hello Tailwind CSS!
     </div>
   );
 }
 
 export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    // Add any other HTML or JavaScript files you want to include
+  ],
+  darkMode: 'media', // Change this to 'media' or remove it
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
