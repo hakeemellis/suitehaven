686ea5a9119c79ff80a1582c5fd205533c28586c
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-02-28 15:42:45 -0500

    Updated

--- src/components/logoutscreen.js
+++ src/components/logoutscreen.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LogoutScreen = () => {
   return (
     <section className={`dark:bg-zinc-950 dark:text-white bg-white text-black p-4 
-        transition-all duration-500 ease-in-out container mx-auto flex items-center justify-center 
+        transition-all duration-500 ease-in-out mx-auto flex items-center justify-center 
         shadow-md m-0 min-h-screen`}>
       <div className="text-center container">
         <h1 className="text-6xl font-bold mb-4">Logout</h1>
